4d0cbd4445660cca41800a4609d4d5134d6fc406
Alan Baines <abaines@vt.edu>
2020-03-08 13:02:42 -0400

    Update spawn.spawning.js
    remove Game.spawns['Spawn1'] references

--- spawn.spawning.js
+++ spawn.spawning.js
@@ -8,7 +8,7 @@ function spawnCreep(spawn, role, body)
 {
 	var newName = role + Game.time;
 
-	var spawnReturn = Game.spawns['Spawn1'].spawnCreep(body, newName,
+	var spawnReturn = spawn.spawnCreep(body, newName,
 		{
 			memory:
 			{
@@ -52,76 +52,82 @@ function bodyScan(creep, scan_type)
 	return false;
 }
 
+function spawnLogic(spawn, harvesters)
+{
+	var energyCapacityAvailable = spawn.room.energyCapacityAvailable;
+	var energyAvailable = spawn.room.energyAvailable;
+
+	if (harvesters.length < 3 * 7 && energyAvailable >= 2300 && energyCapacityAvailable < Infinity)
+	{
+		spawnCreep(spawn, 'harvester',
+			[WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, // 14
+				CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, // 8
+				MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]// 10
+		);
+	}
+	else if (harvesters.length < 11 && energyAvailable >= 1200 && energyCapacityAvailable < 2300)
+	{
+		spawnCreep(spawn, 'harvester',
+			[WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]);
+	}
+	else if (harvesters.length < 10 && energyAvailable >= 800 && energyCapacityAvailable < 1200)
+	{
+		spawnCreep(spawn, 'harvester',
+			[WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
+	}
+	else if (harvesters.length < 9 && energyAvailable >= 550 && energyCapacityAvailable < 800)
+	{
+		spawnCreep(spawn, 'harvester',
+			[WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE]);
+	}
+	else if (harvesters.length < 5 && energyAvailable >= 200 && energyCapacityAvailable < 550)
+	{
+		spawnCreep(spawn, 'harvester', [WORK, CARRY, MOVE]);
+	}
+	else if (harvesters.length < 3 && energyAvailable >= 200)
+	{
+		spawnCreep(spawn, 'harvester', [WORK, CARRY, MOVE]);
+	}
+
+	if (false)
+	{
+		var claimers = 0;
+		for (var name in Game.creeps)
+		{
+			var creep = Game.creeps[name];
+			if (bodyScan(creep, "claim"))
+			{
+				claimers = 1 + claimers
+			}
+		}
+		if (claimers == 0 && energyAvailable >= 900 && energyCapacityAvailable < Infinity)
+		{
+			spawnCreep(spawn, 'claimer', [CLAIM, WORK, CARRY, MOVE, MOVE, MOVE]);
+		}
+	}
+
+	if (spawn.spawning)
+	{
+		var spawningCreep = Game.creeps[spawn.spawning.name];
+		spawn.room.visual.text(
+			'🛠️' + spawningCreep.memory.role,
+			spawn.pos.x + 1,
+			spawn.pos.y,
+		{
+			align: 'left',
+			opacity: 0.8
+		}
+		);
+	}
+}
+
 var spawnSpawning =
 {
 	run: function ()
 	{
 		var harvesters = roleHarvester.getHarvesters();
-		var energyCapacityAvailable = Game.spawns['Spawn1'].room.energyCapacityAvailable;
-		var energyAvailable = Game.spawns['Spawn1'].room.energyAvailable;
 
-		if (harvesters.length < 3 * 7 && energyAvailable >= 2300 && energyCapacityAvailable < Infinity)
-		{
-			spawnCreep(Game.spawns['Spawn1'], 'harvester',
-				[WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, // 14
-					CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, // 8
-					MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]// 10
-			);
-		}
-		else if (harvesters.length < 11 && energyAvailable >= 1200 && energyCapacityAvailable < 2300)
-		{
-			spawnCreep(Game.spawns['Spawn1'], 'harvester',
-				[WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]);
-		}
-		else if (harvesters.length < 10 && energyAvailable >= 800 && energyCapacityAvailable < 1200)
-		{
-			spawnCreep(Game.spawns['Spawn1'], 'harvester',
-				[WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
-		}
-		else if (harvesters.length < 9 && energyAvailable >= 550 && energyCapacityAvailable < 800)
-		{
-			spawnCreep(Game.spawns['Spawn1'], 'harvester',
-				[WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE]);
-		}
-		else if (harvesters.length < 5 && energyAvailable >= 200 && energyCapacityAvailable < 550)
-		{
-			spawnCreep(Game.spawns['Spawn1'], 'harvester', [WORK, CARRY, MOVE]);
-		}
-		else if (harvesters.length < 3 && energyAvailable >= 200)
-		{
-			spawnCreep(Game.spawns['Spawn1'], 'harvester', [WORK, CARRY, MOVE]);
-		}
-
-		if (false)
-		{
-			var claimers = 0;
-			for (var name in Game.creeps)
-			{
-				var creep = Game.creeps[name];
-				if (bodyScan(creep, "claim"))
-				{
-					claimers = 1 + claimers
-				}
-			}
-			if (claimers == 0 && energyAvailable >= 900 && energyCapacityAvailable < Infinity)
-			{
-				spawnCreep(Game.spawns['Spawn1'], 'claimer', [CLAIM, WORK, CARRY, MOVE, MOVE, MOVE]);
-			}
-		}
-
-		if (Game.spawns['Spawn1'].spawning)
-		{
-			var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-			Game.spawns['Spawn1'].room.visual.text(
-				'🛠️' + spawningCreep.memory.role,
-				Game.spawns['Spawn1'].pos.x + 1,
-				Game.spawns['Spawn1'].pos.y,
-			{
-				align: 'left',
-				opacity: 0.8
-			}
-			);
-		}
+		spawnLogic(Game.spawns['Spawn1'], harvesters);
 	}
 }
 
