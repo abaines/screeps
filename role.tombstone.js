d79e2256ff46776a4dec2138481d007b9f71b86a
Alan Baines <abaines@vt.edu>
2020-03-09 00:22:12 -0400

    ♻️
    creepCollectTombstone

--- role.tombstone.js
+++ role.tombstone.js
@@ -4,9 +4,47 @@
 
 var tombstoneLogic =
 {
+	creepCollectTombstone: function (creep, tombstone)
+	{
+		var withdrawResult = creep.withdraw(tombstone, RESOURCE_ENERGY);
+
+		if (ERR_NOT_IN_RANGE == withdrawResult)
+		{
+			creep.moveTo(tombstone,
+			{
+				visualizePathStyle:
+				{
+					stroke: '#ffffff'
+				}
+			}
+			);
+		}
+		else if (OK == withdrawResult)
+		{
+			console.log("WIN!", 'creepCollectTombstone', JSON.stringify(tombstone.store), tombstone.room.name);
+		}
+		else
+		{
+			console.log('withdrawResult', withdrawResult);
+		}
+		return;
+	},
 	findCreepToGetStore: function (store, pos)
 	{
-		console.log(pos, JSON.stringify(store));
+		var creep = pos.findClosestByPath(FIND_MY_CREEPS,
+			{
+				filter: (creep) =>
+				{
+					var freeCapacity = creep.store.getFreeCapacity(RESOURCE_ENERGY);
+					var isCreepEmpty = creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0;
+					var isConstruction = creep.memory.construction;
+
+					return !isConstruction && (isCreepEmpty || freeCapacity >= store.getUsedCapacity(RESOURCE_ENERGY));
+				}
+			}
+			);
+
+		return creep;
 	},
 	perRoom: function (room)
 	{
@@ -26,7 +64,14 @@ var tombstoneLogic =
 			for (var idx in tombstones)
 			{
 				var tombstone = tombstones[idx];
-				this.findCreepToGetStore(tombstone.store, tombstone.pos);
+				var creep = this.findCreepToGetStore(tombstone.store, tombstone.pos);
+
+				if (creep)
+				{
+					this.creepCollectTombstone(creep, tombstone);
+					creep.say("♻️");
+					return;
+				}
 			}
 		}
 	},
