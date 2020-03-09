ba7ef4e82597734c88d5aaffedbfabba1672ab45
Alan Baines <abaines@vt.edu>
2020-03-08 23:55:12 -0400

    Update role.tombstone.js

--- role.tombstone.js
+++ role.tombstone.js
@@ -4,15 +4,39 @@
 
 var tombstoneLogic =
 {
+	findCreepToGetStore: function (store, pos)
+	{
+		console.log(pos, JSON.stringify(store));
+	},
+	perRoom: function (room)
+	{
+		// FIND_TOMBSTONES
+		// FIND_DROPPED_RESOURCES
+		var tombstones = room.find(FIND_TOMBSTONES,
+			{
+				filter: (tombstone) =>
+				{
+					return tombstone.store[RESOURCE_ENERGY] >= 50;
+				}
+			}
+			);
+
+		if (tombstones.length)
+		{
+			for (var idx in tombstones)
+			{
+				var tombstone = tombstones[idx];
+				this.findCreepToGetStore(tombstone.store, tombstone.pos);
+			}
+		}
+	},
 	run: function ()
 	{
-		var sb = []
 		for (var idx in Game.rooms)
 		{
 			var room = Game.rooms[idx];
-			sb.push(room.name);
+			this.perRoom(room);
 		}
-		console.log(JSON.stringify(sb));
 	},
 };
 
