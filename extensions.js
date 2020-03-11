bc221c2d73b9d36d97e41d4ec3e490489eb6ffca
Alan Baines <abaines@vt.edu>
2020-03-11 00:00:39 -0400

    Update extensions.js
    move doc

--- extensions.js
+++ extensions.js
@@ -3,9 +3,9 @@
 'use strict';
 
 
-// https://stackoverflow.com/a/29494612/
 Number.prototype.toFixedNumber = function (digits, base)
 {
+	// https://stackoverflow.com/a/29494612/
 	var pow = Math.pow(base || 10, digits);
 	return Math.round(this * pow) / pow;
 }
