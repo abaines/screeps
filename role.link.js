d7e27d872e34de6255ab4775a865f742ca14ce19
Alan Baines <abaines@vt.edu>
2020-03-11 01:10:37 -0400

    Update role.link.js
    determineBehaviorOfLink

--- role.link.js
+++ role.link.js
@@ -6,16 +6,41 @@ var linkLogic =
 {
 	determineBehaviorOfLink: function (structureLink)
 	{
-		if (Memory.links[structureLink.id])
+		if (Memory.links[structureLink.id] && Memory.links[structureLink.id].goal)
 		{
 			return;
 		}
 
 		var source = structureLink.pos.findClosestByRange(FIND_SOURCES);
-		var sourceDistance = structureLink.pos.distanceToStructure(source);
-		var controllerDistance = structureLink.pos.distanceToStructure(structureLink.room.controller);
+		var controller = structureLink.room.controller;
 
-		console.log(structureLink, sourceDistance, controllerDistance);
+		var sourceDistance = structureLink.pos.distanceToStructure(source);
+		var controllerDistance = structureLink.pos.distanceToStructure(controller);
+
+		console.log('determineBehaviorOfLink', structureLink, sourceDistance, controllerDistance);
+
+		if (sourceDistance < 5 && controllerDistance > 20)
+		{
+			Memory.links[structureLink.id] =
+			{
+				goal: "sink",
+				source: source.id,
+			}
+			return;
+		}
+		else if (controllerDistance < 5 && sourceDistance > 20)
+		{
+			Memory.links[structureLink.id] =
+			{
+				goal: "fountain",
+				controller: controller.id,
+			}
+			return;
+		}
+		else
+		{
+			console.log("Unable to classify link:", structureLink);
+		}
 	},
 	determineBehavior: function ()
 	{
