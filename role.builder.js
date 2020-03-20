400a58bb9d806dc3b4bfa351d0bf15bf63166a9f
Alan Baines <abaines@vt.edu>
2020-03-19 21:07:35 -0400

    Update role.builder.js
    about to re-write memory entirely for 'builder'

--- role.builder.js
+++ role.builder.js
@@ -6,31 +6,73 @@ const log = require('log').log;
 
 const core =
 {
-	findWithdrawSource: function (creep)
+	findWithdrawStorage: function (creep)
 	{
-		if (creep.room.storage.store[RESOURCE_ENERGY] > 20 * 50)
+		if (creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] > 20 * 50)
 		{
 			return creep.room.storage;
+		}
+		const container = creep.pos.findClosestByPath(FIND_STRUCTURES,
+			{
+				filter: (structure) =>
+				{
+					const structureType = structure.structureType;
+					if (STRUCTURE_CONTAINER == structureType)
+					{
+						const energy = structure.store[RESOURCE_ENERGY];
+						return energy > 50;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			);
+		if (container)
+		{
+			return container;
 		}
 	},
 	runPerCreep: function (creep)
 	{
 		const site = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
 
-		if (creep.store[RESOURCE_ENERGY] == 0)
+		if (creep.store[RESOURCE_ENERGY] == 0 && creep.memory.constructionSite != null)
 		{
-			creep.memory.withDraw = this.findWithdrawSource(creep).id;
-			creep.memory.constructionSite = null;
+			const storage = this.findWithdrawStorage(creep);
+			if (storage)
+			{
+				creep.memory.constructionSite = null;
+				creep.memory.withDraw = storage.id;
+				creep.memory.source = null;
+			}
+			else
+			{
+				const source = creep.pickSource();
+				if (source && source.energy > 50)
+				{
+					creep.memory.constructionSite = null;
+					creep.memory.withDraw = null;
+					creep.memory.source = source.id;
+				}
+			}
 		}
+
 		if (creep.store.getFreeCapacity() == 0)
 		{
 			creep.memory.constructionSite = site && site.id;
 			creep.memory.withDraw = null;
+			creep.memory.source = null;
 		}
 
 		if (creep.memory.withDraw && Game.getObjectById(creep.memory.withDraw))
 		{
 			creep.smartWithdraw(Game.getObjectById(creep.memory.withDraw));
+		}
+		else if (creep.memory.source && Game.getObjectById(creep.memory.source))
+		{
+			creep.smartHarvest(Game.getObjectById(creep.memory.source));
 		}
 		else if (creep.memory.constructionSite && Game.getObjectById(creep.memory.constructionSite))
 		{
@@ -40,6 +82,7 @@ const core =
 		{
 			creep.memory.constructionSite = site && site.id;
 			creep.memory.withDraw = null;
+			creep.memory.source = null;
 		}
 		else
 		{
